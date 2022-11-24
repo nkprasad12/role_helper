cd9e28072241450da727fac1cf9e8a4927adb489
Nitin Prasad <nkprasa@gmail.com>
2022-11-24 10:33:23 -0600

    Stop repeats in allowed users and add formatting.

--- commands/findusers.js
+++ commands/findusers.js
@@ -1,5 +1,9 @@
 const { SlashCommandBuilder, SlashCommandRoleOption } = require('discord.js');
 
+const OPTION_WITH = 'with'
+const OPTION_WITHOUT = 'without'
+
+
 function getRole(guild, roleName) {
     return guild.roles.cache.find(
         role => role.name.localeCompare(roleName, undefined, { sensitivity: 'base' }) === 0)
@@ -13,54 +17,60 @@ function formatMembers(members) {
     return members.map(formatMember).join('\n')
 }
 
+function parseListOption(interaction, key) {
+    return interaction.options.getString(key).split(',').map(s => s.trim())
+}
+
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('findusers')
+    data: new SlashCommandBuilder()
+        .setName('findusers')
         .addStringOption(option =>
-            option.setName('with')
+            option.setName(OPTION_WITH)
                 .setDescription('Name of roles to include (separate with commas)')
                 .setRequired(true))
         .addStringOption(option =>
-            option.setName('without')
+            option.setName(OPTION_WITHOUT)
                 .setDescription('Names of roles to include (separate with commas)')
                 .setRequired(true))
-		.setDescription('Allows finding users by role.'),
-	async execute(interaction) {
-        const allowRoles = interaction.options.getString('with').split(',').map(s => s.trim())
-        const filterRoles = interaction.options.getString('without').split(',').map(s => s.trim())
+        .setDescription('Allows finding users by role.'),
 
-        const filterIds = new Set()
-        for (const roleName of filterRoles) {
+    async execute(interaction) {
+        const rolesToAllow = parseListOption(interaction, OPTION_WITH)
+        const rolesToFilter = parseListOption(interaction, OPTION_WITHOUT)
+
+        const idsToFilter = new Set()
+        for (const roleName of rolesToFilter) {
             role = getRole(interaction.guild, roleName)
             if (role === undefined) {
-                await interaction.reply(`Could not find any role with name ${roleName}`);
+                await interaction.reply(`Could not find any role with name ${roleName}`)
                 return
             }
             role.members.forEach(member => {
-                filterIds.add(member.id)
-            });      
+                idsToFilter.add(member.id)
+            })
         }
 
-        const allowedUsers = []
-        for (const roleName of allowRoles) {
+        const filteredUsers = []
+        for (const roleName of rolesToAllow) {
             role = getRole(interaction.guild, roleName)
             if (role === undefined) {
                 await interaction.reply(`Could not find any role with name ${roleName}`);
                 return
             }
             role.members.forEach(member => {
-                if (filterIds.has(member.id)) {
+                if (idsToFilter.has(member.id)) {
                     return
                 }
-                allowedUsers.push(member)                
-            });       
+                filteredUsers.push(member)
+                idsToFilter.add(member.id)
+            })
         }
 
-        allowed = allowRoles.join('and')
-        filtered = filterRoles.join('or')
-        message = `Found ${allowedUsers.length} users with ${allowed} but not ${filtered}:\n`
-        message += formatMembers(allowedUsers)
-    
-		await interaction.reply({ content: message, ephemeral: true});
-	},
-};
+        allowed = rolesToAllow.join('and')
+        filtered = rolesToFilter.join('or')
+        message = `Found ${filteredUsers.length} users with ${allowed} but not ${filtered}:\n`
+        message += formatMembers(filteredUsers)
+        await interaction.reply({ content: message, ephemeral: true })
+    },
+}
